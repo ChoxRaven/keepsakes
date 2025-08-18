b6161eaa2255e50b9faaf22ce0c786451653438e
Chox Raven <87282469+ChoxRaven@users.noreply.github.com>
2025-08-18 06:52:46 +0400

    initial files

---
+++ src/client/java/net/keepsakes/KeepsakesClient.java
@@ -0,0 +1,10 @@
+package net.keepsakes;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KeepsakesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/net/keepsakes/KeepsakesDataGenerator.java
@@ -0,0 +1,11 @@
+package net.keepsakes;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class KeepsakesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
