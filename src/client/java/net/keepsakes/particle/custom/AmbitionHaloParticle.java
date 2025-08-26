4186d6b43032a09fb6a7af386f6ed12a5c1ea607
Chox Raven <mohdsamir048@gmail.com>
2025-08-26 14:08:05 +0400

    Added a unique particle to the Chrysalis of Eternity that follows the player's head, currently it is not smooth and uses a placeholder texture

---
+++ src/client/java/net/keepsakes/particle/custom/AmbitionHaloParticle.java
@@ -0,0 +1,38 @@
+package net.keepsakes.particle.custom;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.particle.*;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.particle.SimpleParticleType;
+
+@Environment(EnvType.CLIENT)
+public class AmbitionHaloParticle extends AnimatedParticle {
+    AmbitionHaloParticle(ClientWorld world, double x, double y, double z, double velocityX, double velocityY, double velocityZ, SpriteProvider spriteProvider) {
+        super(world, x, y, z, spriteProvider, 0);
+        this.velocityX = velocityX;
+        this.velocityY = velocityY;
+        this.velocityZ = velocityZ;
+        this.scale = 0.5f;
+        this.maxAge = 1;
+        this.setSpriteForAge(spriteProvider);
+    }
+
+    public void move(double dx, double dy, double dz) {
+        this.setBoundingBox(this.getBoundingBox().offset(dx, dy, dz));
+        this.repositionFromBoundingBox();
+    }
+
+    @Environment(EnvType.CLIENT)
+    public static class Factory implements ParticleFactory<SimpleParticleType> {
+        private final SpriteProvider spriteProvider;
+
+        public Factory(SpriteProvider spriteProvider) {
+            this.spriteProvider = spriteProvider;
+        }
+
+        public Particle createParticle(SimpleParticleType simpleParticleType, ClientWorld clientWorld, double d, double e, double f, double g, double h, double i) {
+            return new AmbitionHaloParticle(clientWorld, d, e, f, g, h, i, this.spriteProvider);
+        }
+    }
+}
--- src/main/java/net/keepsakes/Keepsakes.java
+++ src/main/java/net/keepsakes/Keepsakes.java
@@ -2,7 +2,12 @@ package net.keepsakes;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
 import net.keepsakes.item.ModItems;
+import net.minecraft.particle.SimpleParticleType;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,14 +19,24 @@ public class Keepsakes implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+    public static final SimpleParticleType AMBITION_HALO_PARTICLE = FabricParticleTypes.simple();
+
+    public static void initializeParticles() {
+        Registry.register(Registries.PARTICLE_TYPE, Identifier.of(MOD_ID, "ambition_halo_particle"), AMBITION_HALO_PARTICLE);
+    }
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 		// * Initialize Items
-		LOGGER.info("Loading Items");
+		LOGGER.info("Loading Items...");
 		ModItems.initialize();
-		LOGGER.info("Finished Loading!");
+		LOGGER.info("Finished loading items!");
+
+        LOGGER.info("Loading Particles...");
+        initializeParticles();
+        LOGGER.info("Finished loading particles!");
 	}
 }
