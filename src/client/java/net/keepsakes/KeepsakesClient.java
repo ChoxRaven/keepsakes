9b746f12a45f643458ec0673aad8d54a40c01a87
Chox Raven <mohdsamir048@gmail.com>
2025-08-28 17:21:03 +0400

    cleanup, new placeholder texture for the Ambition Halo, added a WIP billboard renderer for the player

--- src/client/java/net/keepsakes/KeepsakesClient.java
+++ src/client/java/net/keepsakes/KeepsakesClient.java
@@ -2,13 +2,21 @@ package net.keepsakes;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.LivingEntityFeatureRendererRegistrationCallback;
 import net.keepsakes.particle.custom.AmbitionHaloParticle;
+import net.keepsakes.rendering.AmbitionBillboardFeatureRenderer;
 import net.minecraft.client.particle.EndRodParticle;
+import net.minecraft.client.render.entity.LivingEntityRenderer;
+import net.minecraft.client.render.entity.PlayerEntityRenderer;
 
 public class KeepsakesClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-        ParticleFactoryRegistry.getInstance().register(Keepsakes.AMBITION_HALO_PARTICLE, AmbitionHaloParticle.Factory::new);
+        LivingEntityFeatureRendererRegistrationCallback.EVENT.register(((entityType, livingEntityRenderer, registrationHelper, context) -> {
+            if (livingEntityRenderer instanceof PlayerEntityRenderer) {
+                registrationHelper.register(new AmbitionBillboardFeatureRenderer((PlayerEntityRenderer) livingEntityRenderer));
+            }
+        }));
     }
 }
---
+++ src/client/java/net/keepsakes/rendering/AmbitionBillboardFeatureRenderer.java
@@ -0,0 +1,66 @@
+    package net.keepsakes.rendering;
+
+    import net.keepsakes.Keepsakes;
+    import net.minecraft.client.MinecraftClient;
+    import net.minecraft.client.render.Camera;
+    import net.minecraft.client.render.RenderLayer;
+    import net.minecraft.client.render.VertexConsumer;
+    import net.minecraft.client.render.VertexConsumerProvider;
+    import net.minecraft.client.render.entity.feature.FeatureRenderer;
+    import net.minecraft.client.render.entity.feature.FeatureRendererContext;
+    import net.minecraft.client.util.math.MatrixStack;
+    import net.minecraft.entity.Entity;
+    import net.minecraft.util.Identifier;
+    import net.minecraft.util.math.RotationAxis;
+    import org.joml.Vector3f;
+    import org.slf4j.Logger;
+    import org.slf4j.LoggerFactory;
+
+    public class AmbitionBillboardFeatureRenderer extends FeatureRenderer {
+        public static final Logger LOGGER = LoggerFactory.getLogger(Keepsakes.MOD_ID);
+        private static final Identifier TEXTURE = Identifier.of(Keepsakes.MOD_ID, "textures/particle/ambition_halo_texture.png");
+
+        public AmbitionBillboardFeatureRenderer(FeatureRendererContext context) {
+            super(context);
+        }
+
+        @Override
+        public void render(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, Entity entity, float limbAngle, float limbDistance, float tickDelta, float animationProgress, float headYaw, float headPitch) {
+            // TODO : Implement function to check when this should be drawn on players
+            boolean shouldRender = true;
+
+            if(shouldRender) {
+                int overlay = 0;
+                matrices.push();
+
+                // Translate to the player's head
+                matrices.translate(0.0D, entity.getHeight() - 2f, 0.0D);
+
+                // Client camera
+                Camera camera = MinecraftClient.getInstance().gameRenderer.getCamera();
+
+                // TODO : Make this actually fucking work because mojang seems to make the body rotation hardcoded
+                //matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(-entity.getBodyYaw()));
+
+                // Apply rotation
+                matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(camera.getYaw()));
+
+                // Draw the billboard
+                matrices.scale(0.5f, 0.5f, 0.5f);
+
+                VertexConsumer vertexConsumer = vertexConsumers.getBuffer(RenderLayer.getEntityCutoutNoCull(TEXTURE));
+
+                Vector3f normal = new Vector3f(0.0f, 0.0f, 1.0f);
+                normal.mul(matrices.peek().getNormalMatrix());
+
+                vertexConsumer.vertex(matrices.peek().getPositionMatrix(), -1.0f, -1.0f, 0.0f).color(1.0f, 1.0f, 1.0f, 1.0f).texture(0.0f, 1.0f).overlay(overlay).light(light).normal(normal.x, normal.y, normal.z);
+                vertexConsumer.vertex(matrices.peek().getPositionMatrix(), -1.0f, 1.0f, 0.0f).color(1.0f, 1.0f, 1.0f, 1.0f).texture(0.0f, 0.0f).overlay(overlay).light(light).normal(normal.x, normal.y, normal.z);
+                vertexConsumer.vertex(matrices.peek().getPositionMatrix(), 1.0f, 1.0f, 0.0f).color(1.0f, 1.0f, 1.0f, 1.0f).texture(1.0f, 0.0f).overlay(overlay).light(light).normal(normal.x, normal.y, normal.z);
+                vertexConsumer.vertex(matrices.peek().getPositionMatrix(), 1.0f, -1.0f, 0.0f).color(1.0f, 1.0f, 1.0f, 1.0f).texture(1.0f, 1.0f).overlay(overlay).light(light).normal(normal.x, normal.y, normal.z);
+
+                matrices.pop();
+            }
+
+
+        }
+    }
