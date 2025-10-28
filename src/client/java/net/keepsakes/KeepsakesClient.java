53ad4cb1f66d5809706a4359edbd1bad1eead0ef
Chox Raven <mohdsamir048@gmail.com>
2025-10-28 14:31:34 +0400

    yur

--- src/client/java/net/keepsakes/KeepsakesClient.java
+++ src/client/java/net/keepsakes/KeepsakesClient.java
@@ -8,7 +8,7 @@ import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
 public class KeepsakesClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		BlockEntityRendererFactories.register(ModBlockEntities.DEMATERIALIZED_BLOCK_ENTITY, DematerializedBlockEntityRenderer::new);
+        //BlockEntityRendererFactories.register(ModBlockEntities.DEMATERIALIZED_BLOCK_ENTITY, DematerializedBlockEntityRenderer::new);
 //        LivingEntityFeatureRendererRegistrationCallback.EVENT.register(((entityType, livingEntityRenderer, registrationHelper, context) -> {
 //            if (livingEntityRenderer instanceof PlayerEntityRenderer) {
 //                registrationHelper.register(new AmbitionBillboardFeatureRenderer((PlayerEntityRenderer) livingEntityRenderer));
